5482322d813ae9aee78975a57ef111ed1dfef0d8
eranelbaz97 <eranelbaz97+github@gmail.com>
2025-08-21 16:52:12 +0300

    add describe to environment schema

--- src/mcp/schemas/get-environments-params-schema.ts
+++ src/mcp/schemas/get-environments-params-schema.ts
@@ -1,10 +1,19 @@
 import z from 'zod';
 
 export const GetEnvironmentsParamsSchema = z.object({
-  projectId: z.string().optional(),
-  environmentId: z.string().optional(),
-  name: z.string().optional(),
-  limit: z.number().int().positive().max(100).optional(),
+  projectId: z.string().optional().describe('The ID of the project to get environments for'),
+  environmentId: z
+    .string()
+    .optional()
+    .describe('The ID of the environment to get, if specified will ignore other parameters'),
+  name: z.string().optional().describe('The name of the environment to get'),
+  limit: z
+    .number()
+    .int()
+    .positive()
+    .max(100)
+    .optional()
+    .describe('The maximum number of environments to return'),
   offset: z.number().int().nonnegative().optional()
 });
 
